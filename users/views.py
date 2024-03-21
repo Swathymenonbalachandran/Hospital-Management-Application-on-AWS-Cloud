a1b53ec0aadc0a0750d758d3b40d113af36926ad
X23108568 <145655698+swathy-3005@users.noreply.github.com>
2024-03-21 00:32:37 +0000

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,35 @@
+import os
+import random
+import string
+import requests
+from PIL import Image, ImageDraw, ImageFont
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserSignUpForm
-
-
+ 
+def generate_captcha_image(text):
+    # Create a blank image with white background
+    width, height = 200, 50
+    image = Image.new("RGB", (width, height), "white")
+    # Load the default font
+    font = ImageFont.load_default()
+    # Create a draw object
+    draw = ImageDraw.Draw(image)
+    # Calculate the size of the text
+    text_width, text_height = draw.textsize(text, font=font)
+    # Draw the text on the image
+    draw.text(((width - text_width) // 2, (height - text_height) // 2), text, font=font, fill="black")
+    # Save the image to a temporary location
+    image_path = '/tmp/captcha.png'
+    image.save(image_path)
+    return image_path
+ 
 def sign_up(request):
+    captcha_response = requests.get('https://gbi3p3szq1.execute-api.eu-west-1.amazonaws.com/dev1/')
+    #captcha_value = captcha_response.json()['body']
+    print(captcha_response.text)
+    captcha_value = captcha_response.json().get('body')
+    print(captcha_value)
     if request.method == "POST":
         form = UserSignUpForm(request.POST)
         if form.is_valid():
@@ -12,7 +38,7 @@ def sign_up(request):
             messages.success(request,
                 'Account has been successfully created for {}!'.format(un))
             return redirect('sign_in')
-    elif request.method == "GET":
+    else:  
         form = UserSignUpForm()
-    return render(request, 'users/signup.html', {'form': form})
-    
+    captcha_image_url = generate_captcha_image(captcha_value)  # Generate CAPTCHA image
+    return render(request, 'users/signup.html', {'form': form, 'captcha_image_url': captcha_image_url})
